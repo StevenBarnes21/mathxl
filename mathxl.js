cce3020801620cc2f5ccaf7b930d9f889f2db0cb
StevenBarnes21 <ssbarnes21@gmail.com>
2019-12-17 02:41:47 +0000

    added getGeoSeqSeries

--- mathxl.js
+++ mathxl.js
@@ -27,19 +27,39 @@ function findGeoSeqConstant(sequence) {
 
   for (let i = 0; i < sequence.length - 1; i++) {
     if ((sequence[i + 1] / sequence[i]) !== r) {
-      console.log("is null");
        return null;
     }  
   }
   return r;
 }
 
+// This function finds the sum of the nth term in a geometric sequence series. 
+// It will return null if the sequence is not a valid geometric sequence
+function getGeoSeqSeries(sequence, n) {
+
+  if (isGeoSeq(sequence)) {
+    
+    let a = sequence[0];
+    let r = findGeoSeqConstant(sequence);
+
+    return (Math.pow(r, n) - 1) * a / (r - 1);
+
+  }
+
+  return null;
+}
+
+
+
+
+
 //TESTING CODE
 console.log(findGeoSeqConstant([4, 12]));
+console.log(getGeoSeqSeries([10, 20, 40, 80, 160], 3)); //expect 70
 
 
 let geoSeqConstant = findGeoSeqConstant([-2, 4, -8, 16]);
-console.log(geoSeqConstant);
+console.log(geoSeqConstant); //expect -2
 let isThisAGeoSeq = isGeoSeq([1, 3, 9, 27]);
 
 let sixth = geoSeq([4, 12, 36, 108], 6);
